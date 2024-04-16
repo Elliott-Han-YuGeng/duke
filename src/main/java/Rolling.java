9e5e95085cad13e1aa58a3f36c6f2ee8d58b80a4
Admin <Admin@2JBB8G3-MDD.inc.bytedance.com>
2024-04-17 03:11:17 +0800

    Level 1. Echo

--- src/main/java/Rolling.java
+++ src/main/java/Rolling.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Rolling {
     public static void main(String[] args) {
-        String greeting = "____________________________________________________________\n"
-                + "Hello! I'm Your Rolling Bear!\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
+//      Start
+        String greeting = "Hello! I'm Your Rolling Bear!\n"
+                        + "What can I do for you?";
         System.out.println(greeting);
+
+//      User input
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+
+//      Echo
+        while(!line.toLowerCase().equals("bye")){
+            System.out.println(line);
+            line = in.nextLine();
+        }
+
+//      Exit
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
