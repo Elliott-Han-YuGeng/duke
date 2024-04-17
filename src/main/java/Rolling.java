16fc2d8e22c5b66625f5938a3c49e161ea63d47c
Admin <Admin@2JBB8G3-MDD.inc.bytedance.com>
2024-04-17 14:52:25 +0800

    Level-2: Add, List

--- src/main/java/Rolling.java
+++ src/main/java/Rolling.java
@@ -1,23 +1,53 @@
 import java.util.Scanner;
 
 public class Rolling {
-    public static void main(String[] args) {
-//      Start
+    //  Create a todo list
+    private static String[] todoList = new String[1000];
+    private static int loc = 0;
+
+    public static void start() {
         String greeting = "Hello! I'm Your Rolling Bear!\n"
-                        + "What can I do for you?";
+                + "What can I do for you?";
         System.out.println(greeting);
+    }
+
+    public static void addtoList(String[] ss, String s) {
+        ss[loc] = s;
+        System.out.println("added: " + s);
+        loc++;
+    }
+
+    public static void printList(String[] ss) {
+        for (int i = 0; i < loc; i++) {
+            System.out.println(i + 1 + ". " + ss[i]);
+        }
+    }
+
+    public static void exit() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+//  ---------- Main Funciton --------------------------------------------------------
+
+    public static void main(String[] args) {
+        start();
 
 //      User input
         Scanner in = new Scanner(System.in);
         String line = in.nextLine();
 
+        int i = 0;
+
 //      Echo
-        while(!line.toLowerCase().equals("bye")){
-            System.out.println(line);
+        while(!line.toLowerCase().equals("bye")) {
+            if (line.toLowerCase().equals("list")) {
+                printList(todoList);
+            } else {
+                addtoList(todoList, line);
+            }
             line = in.nextLine();
         }
 
-//      Exit
-        System.out.println("Bye. Hope to see you again soon!");
+        exit();
     }
 }
